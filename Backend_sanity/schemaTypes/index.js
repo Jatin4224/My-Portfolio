cf4fc081e4658199e087beea72559f46b6f63ab6
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-07-08 16:56:02 +0530

    Backend setup

---
+++ Backend_sanity/schemaTypes/index.js
@@ -0,0 +1,19 @@
+import abouts from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import testimonials from './testimonials'
+import works from './works'
+import workExperience from './workExperience'
+
+export const schemaTypes = [
+  works,
+  testimonials,
+  brands,
+  abouts,
+  skills,
+  workExperience,
+  experiences,
+  contact,
+]
